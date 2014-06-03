c5267a0e5a07d86e8be4781c3ea272bf4287715a
0xor1 <daniel.robinson.open@gmail.com>
2014-06-03 19:02:16 +0100

    tidy up Base

--- lib/base.dart
+++ lib/base.dart
@@ -5,11 +5,11 @@
 part of controls_and_panels;
 
 abstract class Base{
-  
+
   static const String CLASS = 'cnp-base';
-  static const String FILL = 'fill';
+  static const String FILL = 'cnp-fill';
   static const String CNP_STAGING_ELEMENT_ID = 'cnp-staging-element-id';
-  
+
   static final DivElement _cnpStagingElement = new DivElement()
   ..id = CNP_STAGING_ELEMENT_ID
   ..style.position = 'absolute'
@@ -19,23 +19,24 @@ abstract class Base{
   ..style.border = '0'
   ..style.padding = '0'
   ..style.overflow = 'hidden';
-  
+
   static bool _fillByDefault = false;
-  
+
   static void setFillByDefault(){
     _fillByDefault = true;
   }
-  
-  static void setUnfillByDefault(){
+
+  static void setShrinkByDefault(){
     _fillByDefault = false;
   }
 
-  final DivElement html = new DivElement()
-  ..classes.add(CLASS);
+  final DivElement html = new DivElement();
+
   CssStyleDeclaration get style => html.style;
 
   Base(){
     _baseStyle.insert();
+    addClass(CLASS);
     stage();
     if(_fillByDefault){
       fill();
@@ -45,39 +46,39 @@ abstract class Base{
   void fill(){
     html.classes.add(FILL);
   }
-  
+
   void unfill(){
     html.classes.remove(FILL);
   }
-  
+
   void addClass(dynamic cssClass){
     if(cssClass is EnumValue){
       cssClass = cssClass.toString();
     }
     html.classes.add(cssClass);
   }
-  
+
   void addClasses(List<dynamic> cssClasses){
     cssClasses.forEach((cssClass){
       addClass(cssClass);
     });
   }
-  
+
   void removeClass(dynamic cssClass){
     if(cssClass is EnumValue){
       cssClass = cssClass.toString();
     }
     html.classes.remove(cssClass);
   }
-  
+
   void removeClasses(List<dynamic> cssClasses){
     cssClasses.forEach((cssClass){
       removeClass(cssClass);
     });
   }
-  
+
   void setSize(String width, String height, {String minWidth, String maxWidth, String minHeight, String maxHeight}){
-    html.style
+    style
     ..width = width
     ..minWidth = minWidth == null? width: minWidth
     ..maxWidth = maxWidth == null? width: maxWidth
